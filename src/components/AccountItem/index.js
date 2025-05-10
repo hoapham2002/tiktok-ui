946d0d052c9a96ee2a5a648637594d1f6e24b369
Hoa Pham <hoaphamxuan2002@gmail.com>
2025-05-10 23:46:49 +0700

    Xây dựng Logic phần Header-2

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,24 +1,28 @@
 import classNames from "classnames/bind";
 import styles from "./AccountItem.module.scss";
 import Image from "~/components/Image";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
-function AccountItem() {
+
+function AccountItem({ data }) {
   return (
-    <div className={cx("wrapper")}>
+    <Link to={`/@${data.nickname}`} className={cx("wrapper")}>
       <Image
         className={cx("avatar")}
-        src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/7322974625531428866~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=da08a0eb&x-expires=1744866000&x-signature=ibr7Sr8vsjYkiTX%2Bc1yfRUicFUQ%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my"
-        alt=""
+        src={data.avatar}
+        alt={data.full_name}
       ></Image>
       <div className={cx("info")}>
         <h4 className={cx("name")}>
-          <span>Nguyen Van A</span>
-          <i className={`fa-solid fa-circle-check ${cx("check")}`}></i>
+          <span>{data.full_name}</span>
+          {data.tick && (
+            <i className={`fa-solid fa-circle-check ${cx("check")}`}></i>
+          )}
         </h4>
-        <span className={cx("username")}>nguyenvana</span>
+        <span className={cx("username")}>{data.nickname}</span>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -12,14 +12,32 @@ function Search() {
   const [searchValue, setSearchValue] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [showResult, setShowResult] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const inputRef = useRef();
 
   useEffect(() => {
-    setTimeout(() => {
-      setSearchResult([1, 1, 1, 1]);
-    }, 0);
-  }, []);
+    if (!searchValue.trim()) {
+      setSearchResult([]);
+      return;
+    }
+
+    setLoading(true);
+
+    fetch(
+      `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
+        searchValue
+      )}&type=less`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setSearchResult(res.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, [searchValue]);
 
   const handleClear = () => {
     setSearchValue("");
@@ -38,10 +56,9 @@ function Search() {
         <div className={cx("search-result")} tabIndex="-1" {...attrs}>
           <PopperWrapper>
             <h4 className={cx("search-title")}>Account</h4>
-            <AccountItem />
-            <AccountItem />
-            <AccountItem />
-            <AccountItem />
+            {searchResult.map((result) => (
+              <AccountItem key={result.id} data={result} />
+            ))}
           </PopperWrapper>
         </div>
       )}
@@ -56,7 +73,7 @@ function Search() {
           onChange={(e) => setSearchValue(e.target.value)}
           onFocus={() => setShowResult(true)}
         />
-        {!!searchValue && (
+        {!!searchValue && !loading && (
           <button
             className={cx("clear")}
             onClick={() => {
@@ -66,7 +83,7 @@ function Search() {
             <i className="fa-solid fa-circle-xmark"></i>
           </button>
         )}
-        {/* <i className={`fa-solid fa-spinner ${cx("loading")}`}></i> */}
+        {loading && <i className={`fa-solid fa-spinner ${cx("loading")}`}></i>}
 
         <button className={cx("search-btn")}>
           <SearchIcon></SearchIcon>
