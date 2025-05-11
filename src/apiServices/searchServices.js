1cdfe198e189a2baa065cf42f12463961f938aca
Hoa Pham <hoaphamxuan2002@gmail.com>
2025-05-11 11:37:47 +0700

    Tìm hiểu thư viện Axios và tái cấu trúc fetch api

---
+++ src/apiServices/searchServices.js
@@ -0,0 +1,19 @@
+import * as request from "~/utils/request";
+
+export const search = async (q, type = "less") => {
+  try {
+    const res = await request.get(
+      // `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`,
+      `users/search`,
+      {
+        params: {
+          q,
+          type,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
